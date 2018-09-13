6e1962f6c17f0b5aa49e9b77789257cf4318ee90
Alex <alexander.emland@elev.ga.ntig.se>
2018-09-13 13:27:28 +0200

    Add assets

---
+++ data/maps/level1/grassHillLeft.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.08.22" name="grassHillLeft" tilewidth="70" tileheight="70" tilecount="1" columns="1">
+ <image source="../../../Downloads/Platformer_assets/Platformer assets (1330 assets)/Base pack (360 assets)/PNG/Tiles/grassHillLeft.png" width="70" height="70"/>
+</tileset>
