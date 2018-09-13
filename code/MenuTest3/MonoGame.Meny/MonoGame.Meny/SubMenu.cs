9b6d71d7e65e0b65d1651b709a223f5e0a19b29f
VictorOlofsson <victor.olofsson@elev.ga.ntig.se>
2018-09-13 14:10:33 +0200

    MenuTest3

---
+++ code/MenuTest3/MonoGame.Meny/MonoGame.Meny/SubMenu.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace MonoGame.Meny
+{
+    internal class SubMenu
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public string Text { get; set; }
+        public bool Selected { get; set; }
+
+        public Action ClickAction { get; set; }
+        public Rectangle HitBox { get; set; }
+        public object Choices { get; internal set; }
+    }
+}
