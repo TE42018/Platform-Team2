675dc1e11eb31423bc7eeb9adbf9ca1940929bf4
JonatanW <jonatan.walett@elev.ga.ntig.se>
2018-09-17 09:48:52 +0200

    GameScreens
    GameScreens

---
+++ GameScreens/GameScreen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace GameScreens
+{
+    public abstract class GameScreen
+    {
+        protected bool initialized;
+        public bool Initialized
+        {
+            get
+            {
+                return initialized;
+            }
+        }
+
+        protected bool quit;
+        public bool Quit
+        {
+            get
+            {
+                return quit;
+            }
+        }
+
+        public virtual void Initialize()
+        {
+            initialized = true;
+        }
+
+        public virtual void LoadContent(GraphicsDevice pGraphicsDevice)
+        {
+
+        }
+
+        public virtual void Update(GameTime pGameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch pSpriteBatch)
+        {
+
+        }
+    }
+}
---
+++ GameScreens/StartupGameScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace GameScreens
+{
+    public class StartupGameScreen : GameScreen
+    {
+        public StartupGameScreen()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent(GraphicsDevice pGraphicsDevice)
+        {
+            base.LoadContent(pGraphicsDevice);
+        }
+
+        public override void Update(GameTime pGameTime)
+        {
+            base.Update(pGameTime);
+        }
+
+        public override void Draw(SpriteBatch pSpriteBatch)
+        {
+            base.Draw(pSpriteBatch);
+        }
+    }
+}
